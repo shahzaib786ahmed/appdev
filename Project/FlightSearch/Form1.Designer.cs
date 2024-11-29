4e2ecb82816f08dc1de89955d53c4275cea3a54b
shahzaib786ahmed <ahmedshahzaib203@gmail.com>
2024-11-29 14:29:11 -0500

    Initial Project

---
+++ Project/FlightSearch/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace FlightSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            dateTimePicker1 = new DateTimePicker();
+            label3 = new Label();
+            comboBox2 = new ComboBox();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            tabPage2 = new TabPage();
+            dateTimePicker2 = new DateTimePicker();
+            button1 = new Button();
+            comboBox3 = new ComboBox();
+            label4 = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(555, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(212, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Flights Around the World";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(60, 136);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1276, 310);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label4);
+            tabPage1.Controls.Add(comboBox3);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(dateTimePicker2);
+            tabPage1.Controls.Add(dateTimePicker1);
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(comboBox2);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(comboBox1);
+            tabPage1.Location = new Point(4, 34);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1268, 272);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(6, 86);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(149, 31);
+            dateTimePicker1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(161, 3);
+            label3.Name = "label3";
+            label3.Size = new Size(34, 25);
+            label3.TabIndex = 3;
+            label3.Text = "To:";
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Montreal, Canada (YUL)", "London, UK (LHR)", "Dubai, UAE (DXB)", "Mexico City, Mexico (MEX)", "Beijing, China (PEK)", "Punta Cana, Dominican Republic (PUJ)" });
+            comboBox2.Location = new Point(172, 31);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(145, 33);
+            comboBox2.TabIndex = 2;
+            comboBox2.Text = "Select City";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 3);
+            label2.Name = "label2";
+            label2.Size = new Size(58, 25);
+            label2.TabIndex = 1;
+            label2.Text = "From:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Montreal, Canada (YUL)", "London, UK (LHR)", "Dubai, UAE (DXB)", "Mexico City, Mexico (MEX)", "Beijing, China (PEK)", "Punta Cana, Dominican Republic (PUJ)" });
+            comboBox1.Location = new Point(6, 31);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(149, 33);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "Select City";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 34);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1268, 236);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(172, 86);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(145, 31);
+            dateTimePicker2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(172, 214);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 34);
+            button1.TabIndex = 6;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "1", "2", "3", "4", "5" });
+            comboBox3.Location = new Point(6, 156);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(189, 33);
+            comboBox3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 128);
+            label4.Name = "label4";
+            label4.Size = new Size(191, 25);
+            label4.TabIndex = 8;
+            label4.Text = "Number of Passengers";
+            label4.Click += label4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1379, 776);
+            Controls.Add(tabControl1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "FlightSearch";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label3;
+        private ComboBox comboBox2;
+        private Label label2;
+        private ComboBox comboBox1;
+        private TabPage tabPage2;
+        private DateTimePicker dateTimePicker1;
+        private Label label4;
+        private ComboBox comboBox3;
+        private Button button1;
+        private DateTimePicker dateTimePicker2;
+    }
+}
---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Project
+{
+    partial class AgencyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(197, 59);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(60, 20);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "AirCR7";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(171, 126);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(220, 26);
+            this.emailTextBox.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(171, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(220, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(329, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Enter Password:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 33);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 33);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "English",
+            "French",
+            "Spanish"});
+            this.comboBox1.Location = new System.Drawing.Point(357, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 28);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Text = "Languages";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Customer",
+            "Admin"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 22);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(118, 28);
+            this.comboBox2.TabIndex = 12;
+            this.comboBox2.Text = "User Mode";
+            // 
+            // AgencyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 340);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "AgencyForm";
+            this.Text = "AirCR7";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
